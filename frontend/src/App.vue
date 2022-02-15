7eda2778b51c16ed4bf1f815541f706be04aa371
Neurologia <patrickkwon777@gmail.com>
2022-02-15 10:14:46 +0700

    'added tailwind'

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld class="bg-red-100" msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
